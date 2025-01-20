b88c824f30c62070a939d8ef65934e3e0a98e509
bhargavi215151 <bhargavi215151@users.noreply.github.com>
2025-01-20 17:34:40 +0530

    latest update

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -7,15 +7,23 @@ const Countries = () => {
   const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
-   
-        fetch( "https://restcountries.com/v3.1/all"
-        ).then((res) => res.json())
-        .then((data) => setCountries(data))
-        .catch((error) => console.error("Error fetching data:", error));
+    const fetchCountries = async () =>{
+      try{
+      const res = await fetch('https://countries-search-data-prod-812920491762.asia-south1.run.app/countries');
+      const data = await res.json();
+      console.log(data);
+      setCountries(data);
+   }
+   catch(err){
+      console.log('error fetching countries', err)
+   }
+  };
+
+  fetchCountries();
   },[]);
 
   const filteredCountries = countries.filter(country =>
-    country.name.common.toLowerCase().includes(searchText.toLowerCase())
+    country.common.toLowerCase().includes(searchText.toLowerCase())
   );
 
   return(
@@ -31,9 +39,9 @@ const Countries = () => {
         {filteredCountries.length > 0 ? (
           filteredCountries.map((country) => (
             <CountryCard 
-              key={country.cca3} 
-              name={country.name.common} 
-              flag={country.flags.png} 
+              key={country.common} 
+              name={country.common} 
+              flag={country.png} 
             />
           ))
         ) : (
