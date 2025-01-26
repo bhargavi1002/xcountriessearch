bdea44b2a41947180c4c083c954774c919eb53f2
bhargavi215151 <bhargavi215151@users.noreply.github.com>
2025-01-26 20:36:52 +0530

    commit new

--- src/CountryCard.jsx
+++ src/CountryCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CountryCard = ({ name, flag }) => {
   return (
     <div className="countryCard">
-      <img src={flag} alt={`${name}'s flag`} />
+      <img src={name.flags.png} alt={`${name}'s flag`} />
       <p>{name}</p>
     </div>
   );
